858e52664b727553cde586d6d9b286b30ca7f07e
Leonardo Micelli <leonardo.micelli@studio.unibo.it>
2022-03-21 12:35:52 +0100

    ex 3

--- src/main/scala/u04lab/polyglot/a01a/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a01a/LogicsImpl.scala
@@ -1,8 +1,10 @@
 package u04lab.polyglot.a01a
 import Logics.*
-
+import u04lab.code.List.*
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01a/sol2/ */
 class LogicsImpl(private val size: Int, private val boat: Int) extends Logics:
+  //1. creo nave random
+  //.2 implemento hit
 
   def hit(row: Int, col: Int) =
     Result.HIT
